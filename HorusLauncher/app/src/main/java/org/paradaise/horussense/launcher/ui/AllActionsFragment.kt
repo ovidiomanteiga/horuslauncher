d18c6a56d1110aaf8b669ab271bcfa9817888b98
Ovidio Manteiga Moar <ovidio.manteiga@live.com>
2018-06-07 21:58:39 +0200

    PAAA #34 > TDD #127 + COD #56: Added UI tests for launching action on tap
    - Also implemented basic launching functionality to make the tests pass.
    - Removed launcher from the list of available apps.
    - Make the app not visible on recent activities list.

--- HorusLauncher/app/src/main/java/org/paradaise/horussense/launcher/ui/AllActionsFragment.kt
+++ HorusLauncher/app/src/main/java/org/paradaise/horussense/launcher/ui/AllActionsFragment.kt
@@ -10,6 +10,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import kotlinx.android.synthetic.main.fragment_all_actions.view.*
 import kotlinx.android.synthetic.main.item_app.view.*
 import org.paradaise.horussense.launcher.R
@@ -17,6 +18,7 @@ import org.paradaise.horussense.launcher.domain.AllActions
 import org.paradaise.horussense.launcher.domain.GetAllActionsInteractor
 import org.paradaise.horussense.launcher.domain.HorusAction
 import org.paradaise.horussense.launcher.infrastructure.AllAppsRepository
+import org.paradaise.horussense.launcher.infrastructure.App
 
 
 
@@ -34,13 +36,26 @@ class AllActionsFragment : Fragment() {
         val rootView = inflater.inflate(R.layout.fragment_all_actions, container, false)
 	    val recyclerView = rootView.recyclerView
 	    val actions = this.interactor.allActions
-	    recyclerView.adapter = ActionsAdapter(actions)
+	    recyclerView.adapter = ActionsAdapter(actions, this::onActionClicked)
 	    val layoutManager = recyclerView.layoutManager
 	    if (layoutManager is GridLayoutManager) {
 		    layoutManager.spanCount = 4
 	    }
         return rootView
     }
+
+
+	private fun onActionClicked(action: HorusAction) {
+		if (action is App) {
+			action.intent?.let {
+				this.startActivity(it)
+				return
+			}
+		}
+		Toast.makeText(this.context,
+				"Action clicked: " + action.name,
+				Toast.LENGTH_LONG).show()
+	}
 
 
 	private lateinit var interactor: GetAllActionsInteractor
@@ -53,16 +68,23 @@ class AllActionsFragment : Fragment() {
 
 private class ActionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    fun bind(item: HorusAction) = with(itemView) {
+    fun bind(item: HorusAction, listener: (HorusAction) -> Unit) = with(itemView) {
         itemView.textView.text = item.name
 	    itemView.imageView.setImageDrawable(item.icon)
+	    itemView.setOnClickListener { listener(item) }
     }
 
 }
 
 
 
-private class ActionsAdapter(val items: AllActions) : RecyclerView.Adapter<ActionViewHolder>() {
+private class ActionsAdapter() : RecyclerView.Adapter<ActionViewHolder>() {
+
+	constructor(items: AllActions, listener: (HorusAction) -> Unit) : this() {
+		this.items = items
+		this.listener = listener
+	}
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActionViewHolder {
         val v = LayoutInflater.from(parent.context)
@@ -77,8 +99,12 @@ private class ActionsAdapter(val items: AllActions) : RecyclerView.Adapter<Actio
 
 
     override fun onBindViewHolder(holder: ActionViewHolder, position: Int) {
-        holder.bind(this.items[position])
+        holder.bind(this.items[position], this.listener)
     }
+
+
+	private lateinit var items: AllActions
+	private lateinit var listener: (HorusAction) -> Unit
 
 }
 
