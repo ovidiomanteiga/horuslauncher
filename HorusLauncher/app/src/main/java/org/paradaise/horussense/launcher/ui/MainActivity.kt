2fcd0198be048a298707b13b3c578c8c7034d245
Ovidio Manteiga Moar <ovidio.manteiga@live.com>
2018-06-24 23:21:10 +0200

    IT01 #114 > AB01 #115 > #146: Change app bar style

--- HorusLauncher/app/src/main/java/org/paradaise/horussense/launcher/ui/MainActivity.kt
+++ HorusLauncher/app/src/main/java/org/paradaise/horussense/launcher/ui/MainActivity.kt
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar.*
 import org.paradaise.horussense.launcher.R
 
 
@@ -16,7 +17,7 @@ class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		this.setContentView(R.layout.activity_main)
-		this.setSupportActionBar(toolbar)
+		this.setSupportActionBar(this.toolbar)
 		this.mSectionsPagerAdapter = SectionsPagerAdapter(this.supportFragmentManager)
 		container.adapter = this.mSectionsPagerAdapter
 		container.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
