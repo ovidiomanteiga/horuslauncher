e66021370ad5a4f2ebb8cb5181aadeedf229979b
Ovidio Manteiga Moar <ovidio.manteiga@live.com>
2018-06-26 23:55:35 +0200

    IT01 #114 > BUG #151 > #153 + #154: Empty Horus list view and behavior

--- HorusLauncher/app/src/main/java/org/paradaise/horussense/launcher/ui/MainActivity.kt
+++ HorusLauncher/app/src/main/java/org/paradaise/horussense/launcher/ui/MainActivity.kt
@@ -7,37 +7,59 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.toolbar.*
 import org.paradaise.horussense.launcher.R
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), HorusListFragmentListener {
+
+	// region Lifecycle
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		this.setContentView(R.layout.activity_main)
 		this.setSupportActionBar(this.toolbar)
 		this.mSectionsPagerAdapter = SectionsPagerAdapter(this.supportFragmentManager)
-		container.adapter = this.mSectionsPagerAdapter
-		container.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
-		tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(container))
+		this.container.adapter = this.mSectionsPagerAdapter
+		this.container.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(this.tabs))
+		this.tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(this.container))
 	}
 
 
 	override fun onBackPressed() { }
 
+	// endregion
+	// region HorusListFragmentListener Implementation
 
+	override fun onEmptyHorusList() {
+		this.tabs.getTabAt(this.allActionsTabIndex)?.select()
+		val message = getString(R.string.empty_horus_list)
+		Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+	}
+
+	// endregion
+	// region Private Properties
+
+	private val allActionsTabIndex: Int = 1
 	private var mSectionsPagerAdapter: SectionsPagerAdapter? = null
 
+	// endregion
+	// region Inner Classes
 
 	inner class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
 		override fun getItem(position: Int): Fragment =
-				if (position == 0) HorusListFragment() else AllActionsFragment()
+				if (position == this@MainActivity.allActionsTabIndex)
+					AllActionsFragment()
+				else
+					HorusListFragment()
 
 		override fun getCount(): Int = 2
 
 	}
+
+	// endregion
 
 }
