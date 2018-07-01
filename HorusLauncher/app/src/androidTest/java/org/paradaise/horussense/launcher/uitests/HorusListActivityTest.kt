c4b2bcdaf0413471479811f6f3be877f9c260008
Ovidio Manteiga Moar <ovidio.manteiga@live.com>
2018-07-01 20:08:32 +0200

    IT01 #114 > AB01 #115 > TDD #157: Tests for Horus list second ordering criteria

--- HorusLauncher/app/src/androidTest/java/org/paradaise/horussense/launcher/uitests/HorusListActivityTest.kt
+++ HorusLauncher/app/src/androidTest/java/org/paradaise/horussense/launcher/uitests/HorusListActivityTest.kt
@@ -15,7 +15,6 @@ import junit.framework.Assert.assertEquals
 import junit.framework.Assert.assertTrue
 import org.junit.*
 import org.junit.runner.RunWith
-import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import org.mockito.Mockito.doNothing
@@ -23,8 +22,8 @@ import org.mockito.MockitoAnnotations
 import org.paradaise.horussense.launcher.composition.MainFactory
 import org.paradaise.horussense.launcher.composition.MainInjector
 import org.paradaise.horussense.launcher.domain.*
-import org.paradaise.horussense.launcher.infrastructure.App
 import org.paradaise.horussense.launcher.ui.HorusListActivity
+import java.util.*
 
 
 /**
@@ -52,6 +51,8 @@ class HorusListActivityTest {
 	private lateinit var action2: HorusListItem
 	@Mock
 	private lateinit var action3: HorusListItem
+	@Mock
+	private lateinit var action4: HorusListItem
 
 	// endregion
 	// region Setup
@@ -63,13 +64,21 @@ class HorusListActivityTest {
 	init {
 		MockitoAnnotations.initMocks(this)
 		MainInjector.setFactory(this.factory)
+		val now = Calendar.getInstance().time
+		val yesterdayCalendar = Calendar.getInstance()
+		yesterdayCalendar.add(Calendar.DATE, -1)
+		val yesterday = yesterdayCalendar.time
 		`when`(this.action1.name).thenReturn("Calendar")
 		`when`(this.action1.numberOfExecutionsLastWeek).thenReturn(3)
 		`when`(this.action2.name).thenReturn("Phone")
 		`when`(this.action2.numberOfExecutionsLastWeek).thenReturn(2)
+		`when`(this.action2.lastExecutionMoment).thenReturn(yesterday)
 		`when`(this.action3.name).thenReturn("Facebook")
 		`when`(this.action3.numberOfExecutionsLastWeek).thenReturn(1)
-		val horusList = listOf(this.action1, this.action2, this.action3)
+		`when`(this.action4.name).thenReturn("Photos")
+		`when`(this.action4.numberOfExecutionsLastWeek).thenReturn(2)
+		`when`(this.action4.lastExecutionMoment).thenReturn(now)
+		val horusList = listOf(this.action1, this.action4, this.action2, this.action3)
 		doNothing().`when`(this.getHorusListInteractor).perform()
 		`when`(this.getHorusListInteractor.horusList).thenReturn(horusList)
 		`when`(this.executeActionInteractor.perform()).then {
@@ -108,12 +117,9 @@ class HorusListActivityTest {
 		val item0Title = item0.getChild(UiSelector().resourceId(textViewRID))
 		val item1 = items.getChildByInstance(UiSelector().resourceId(horusItemRID), 1)
 		val item1Title = item1.getChild(UiSelector().resourceId(textViewRID))
-		val item2 = items.getChildByInstance(UiSelector().resourceId(horusItemRID), 2)
-		val item2Title = item2.getChild(UiSelector().resourceId(textViewRID))
 		assertTrue(count > 0)
 		assertEquals("Calendar", item0Title.text)
-		assertEquals("Phone", item1Title.text)
-		assertEquals("Facebook", item2Title.text)
+		assertEquals("Photos", item1Title.text)
 	}
 
 
@@ -131,6 +137,26 @@ class HorusListActivityTest {
 		item.click()
 		this.mDevice.waitForIdle()
 		Assert.assertNotEquals(this.appPackageName, this.mDevice.currentPackageName)
+	}
+
+
+	@Test
+	fun testListOrdering() {
+		val items = UiScrollable(UiSelector().className(RecyclerView::class.java))
+		val horusItemRID = this.appPackageName + ":id/horusItem"
+		val textViewRID = this.appPackageName + ":id/titleView"
+		val item0 = items.getChildByInstance(UiSelector().resourceId(horusItemRID), 0)
+		val item0Title = item0.getChild(UiSelector().resourceId(textViewRID))
+		val item1 = items.getChildByInstance(UiSelector().resourceId(horusItemRID), 1)
+		val item1Title = item1.getChild(UiSelector().resourceId(textViewRID))
+		val item2 = items.getChildByInstance(UiSelector().resourceId(horusItemRID), 2)
+		val item2Title = item2.getChild(UiSelector().resourceId(textViewRID))
+		val item3 = items.getChildByInstance(UiSelector().resourceId(horusItemRID), 3)
+		val item3Title = item3.getChild(UiSelector().resourceId(textViewRID))
+		assertEquals("Calendar", item0Title.text)
+		assertEquals("Photos", item1Title.text)
+		assertEquals("Phone", item2Title.text)
+		assertEquals("Facebook", item3Title.text)
 	}
 
 	// endregion
--- HorusLauncher/app/src/test/java/org/paradaise/horussense/launcher/unittests/GetHorusListUnitTest.kt
+++ HorusLauncher/app/src/test/java/org/paradaise/horussense/launcher/unittests/GetHorusListUnitTest.kt
@@ -123,6 +123,34 @@ class GetHorusListUnitTest {
 		assertEquals(action1time, predictedActions[1].action)
 	}
 
+
+	@Test
+	fun testPredictedActionsOrderedByFrequencyAndLastExecution() {
+		// Arrange
+		val now = Calendar.getInstance().time
+		val yesterdayCalendar = Calendar.getInstance()
+		yesterdayCalendar.add(Calendar.DATE, -1)
+		val yesterday = yesterdayCalendar.time
+		val actionTwice = this.action2
+		val actionOnceYesterday = this.action1
+		val actionOnceNow = this.action3
+		val executions = listOf(
+				ActionExecution(actionTwice, moment = now),
+				ActionExecution(actionOnceYesterday, moment = yesterday),
+				ActionExecution(actionTwice, moment = now),
+				ActionExecution(actionOnceNow, moment = now)
+		)
+		Mockito.`when`(this.repository.all).thenReturn(executions)
+		// Act
+		this.interactor.perform()
+		// Assert
+		val predictedActions = this.interactor.horusList
+		assertEquals(3, predictedActions.size)
+		assertEquals(actionTwice, predictedActions[0].action)
+		assertEquals(actionOnceNow, predictedActions[1].action)
+		assertEquals(actionOnceYesterday, predictedActions[2].action)
+	}
+
 	// endregion
 
 }
