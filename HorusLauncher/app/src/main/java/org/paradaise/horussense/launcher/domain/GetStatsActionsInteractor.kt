50c43eff2fd9f980d103f3a83c72195a9f7cf669
Ovidio Manteiga Moar <ovidio.manteiga@live.com>
2018-07-17 19:28:00 +0200

    RTPD #44 > PPDT #175 > TDD #179 + COD #180: Get stats interactor & its unit tests

---
+++ HorusLauncher/app/src/main/java/org/paradaise/horussense/launcher/domain/GetStatsActionsInteractor.kt
@@ -0,0 +1,66 @@
+
+package org.paradaise.horussense.launcher.domain
+
+
+class GetStatsActionsInteractor : Interactor {
+
+	constructor(repository: LauncherPresentationRepository) {
+		this.repository = repository
+	}
+
+	var stats: LauncherStatsVO? = null
+		private set
+
+	override fun perform() {
+		this.stats = this.buildStats()
+	}
+
+	private fun buildStats(): LauncherStatsVO? {
+		val all = this.repository.all
+		val averageTimeGettingHorusList = this.getAverageTimeGettingHorusList(all)
+		val averageActionTime = this.getAverageActionTime(all)
+		val horusListAccuracy = this.getHorusListAccuracy(all)
+		val stats = LauncherStatsVO()
+		stats.averageActionTime = averageActionTime
+		stats.averageTimeGettingHorusList = averageTimeGettingHorusList
+		stats.horusListAccuracy = horusListAccuracy
+		return stats
+	}
+
+	private fun getAverageTimeGettingHorusList(all: Collection<LauncherPresentationVO>):
+			Milliseconds?
+	{
+		val nonNullValues = all.mapNotNull { it.millisecondsTakenToGetHorusList }
+		val count = nonNullValues.count()
+		if (count == 0) return null
+		val total = nonNullValues.reduce { acc, milliseconds ->
+			acc + milliseconds
+		}
+		return total / count
+	}
+
+	private fun getAverageActionTime(all: Collection<LauncherPresentationVO>): Seconds? {
+		val nonNullValues = all.mapNotNull {
+			val launchTime = it.launchTime ?: return@mapNotNull null
+			val actionTime = it.actionTime ?: return@mapNotNull null
+			 actionTime.time - launchTime.time
+		}
+		val count = nonNullValues.count()
+		if (count == 0) return null
+		val total = nonNullValues.reduce { acc, diff ->
+			acc + diff
+		}
+		return total / count.toDouble() / 1000.0
+	}
+
+	private fun getHorusListAccuracy(all: Collection<LauncherPresentationVO>): Percentage? {
+		val totalHits = all.count {
+			it.result == LauncherPresentationResult.PERFORMED_PREDICTED_ACTION
+		}
+		if (all.count() == 0) return null
+		return totalHits * 100.0 / all.count()
+	}
+
+	private var repository: LauncherPresentationRepository
+
+}
---
+++ HorusLauncher/app/src/test/java/org/paradaise/horussense/launcher/unittests/GetStatsInteractorUnitTest.kt
@@ -0,0 +1,120 @@
+
+package org.paradaise.horussense.launcher.unittests
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
+import org.paradaise.horussense.launcher.domain.*
+import org.paradaise.horussense.launcher.domain.LauncherPresentationResult.*
+import java.util.*
+
+
+/**
+ * Tests related to getting Horus Launcher performance stats.
+ *
+ * See [user story #175 @ VSTS](https://lateaint.visualstudio.com/HorusSense/_workitems/edit/175).
+ */
+@RunWith(MockitoJUnitRunner::class)
+class GetStatsInteractorUnitTest {
+
+	// region Properties
+
+	@InjectMocks
+	private lateinit var interactor: GetStatsActionsInteractor
+
+	@Mock
+	private lateinit var repository: LauncherPresentationRepository
+
+	// endregion
+	// region Setup
+
+	@Before
+	fun setUp() {
+	}
+
+	// endregion
+	// region Tests
+
+	@Test
+	fun testNoStats() {
+		// Arrange
+		// Act
+		this.interactor.perform()
+		// Assert
+		val stats = this.interactor.stats
+		assertNotNull(stats)
+		assertNull(stats!!.averageActionTime)
+		assertNull(stats.averageTimeGettingHorusList)
+		assertNull(stats.horusListAccuracy)
+	}
+
+	@Test
+	fun testStatsWithJust1Presentation() {
+		// Arrange
+		val presentation1 = LauncherPresentationVO()
+		val launchTimeCalendar = Calendar.getInstance()
+		val launchTime = launchTimeCalendar.time
+		launchTimeCalendar.add(Calendar.SECOND, 1)
+		val actionTime = launchTimeCalendar.time
+		presentation1.launchTime = launchTime
+		presentation1.actionTime = actionTime
+		presentation1.millisecondsTakenToGetHorusList = 444
+		presentation1.result = PERFORMED_PREDICTED_ACTION
+		val all = listOf(presentation1)
+		Mockito.`when`(this.repository.all).thenReturn(all)
+		// Act
+		this.interactor.perform()
+		// Assert
+		val stats = this.interactor.stats
+		assertNotNull(stats)
+		assertEquals(1.0, stats!!.averageActionTime)
+		assertEquals(444L, stats.averageTimeGettingHorusList)
+		assertEquals(100.0, stats.horusListAccuracy)
+	}
+
+	@Test
+	fun testStatsWithJustSomePresentations() {
+		// Arrange
+		val presentation1 = this.getLauncherPresentationMock(1, 100, PERFORMED_PREDICTED_ACTION)
+		val presentation2 = this.getLauncherPresentationMock(3, 300, NO_ACTION_PERFORMED)
+		val all = listOf(presentation1, presentation2)
+		Mockito.`when`(this.repository.all).thenReturn(all)
+		// Act
+		this.interactor.perform()
+		// Assert
+		val stats = this.interactor.stats
+		assertNotNull(stats)
+		assertEquals(2.0, stats!!.averageActionTime)
+		assertEquals(200L, stats.averageTimeGettingHorusList)
+		assertEquals(50.0, stats.horusListAccuracy)
+	}
+
+
+	// endregion
+	// region Private Methods
+
+	private fun getLauncherPresentationMock(actionTimeDifference: Int,
+	                                         millisecondsTakenToGetHorusList: Milliseconds,
+	                                         result: LauncherPresentationResult):
+			LauncherPresentationVO
+	{
+		val presentationVO = LauncherPresentationVO()
+		val launchTimeCalendar = Calendar.getInstance()
+		val launchTime = launchTimeCalendar.time
+		launchTimeCalendar.add(Calendar.SECOND, actionTimeDifference)
+		val actionTime = launchTimeCalendar.time
+		presentationVO.launchTime = launchTime
+		presentationVO.actionTime = actionTime
+		presentationVO.millisecondsTakenToGetHorusList = millisecondsTakenToGetHorusList
+		presentationVO.result = result
+		return presentationVO
+	}
+
+	// endregion
+
+}
